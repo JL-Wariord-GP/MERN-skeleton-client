1abb7b712adafad6fbe0f3be24778f3b7fb8920c
Jorge Luis Gomez <63881238+JL-Wariord-GP@users.noreply.github.com>
2023-01-27 22:47:51 -0500

    Update App.js
    update

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ import Customers from "scenes/customers"
 import Transactions from "scenes/transactions"
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
+import Admin from "scenes/admin";
+import Performance from "scenes/performance";
 
 function App() {
 
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
 
 
   return (
@@ -33,6 +37,11 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/performance" element={<Performance />} />
             </Route>
           </Routes>
         </ThemeProvider>
